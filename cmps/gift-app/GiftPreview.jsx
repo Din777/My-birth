3153af4f06639177124c05df77e9eaaf6eca04d9
unknown <din4eg@gmail.com>
2021-07-01 00:32:11 +0300

    bla bla 1

--- cmps/gift-app/GiftPreview.jsx
+++ cmps/gift-app/GiftPreview.jsx
@@ -3,10 +3,10 @@ export function GiftPreview({ gift }) {
     function getPicture() {
         console.log('gift.nickname', gift.nickname);
         console.log('gift.src', gift.src);
-        if (gift.nickname === 'skalka') return "../assets/img/skalka.jpg"
-        else if (gift.nickname === 'havaianas') return "../assets/img/havaianas.jpg"
-        else if (gift.nickname === 'scrub') return "../assets/img/scrub.jpg"
-        else return "../assets/img/spray.jpg"
+        if (gift.nickname === 'skalka') return "./assets/img/skalka.jpg"
+        else if (gift.nickname === 'havaianas') return "./assets/img/havaianas.jpg"
+        else if (gift.nickname === 'scrub') return "./assets/img/scrub.jpg"
+        else return "./assets/img/spray.jpg"
     }
 
     return (
