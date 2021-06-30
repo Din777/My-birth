9e1c6eb1f1e86e521a35b5b3bfbde695e7eabb3e
unknown <din4eg@gmail.com>
2021-07-01 00:26:38 +0300

    bla bla

--- cmps/gift-app/GiftPreview.jsx
+++ cmps/gift-app/GiftPreview.jsx
@@ -6,7 +6,7 @@ export function GiftPreview({ gift }) {
         if (gift.nickname === 'skalka') return "../assets/img/skalka.jpg"
         else if (gift.nickname === 'havaianas') return "../assets/img/havaianas.jpg"
         else if (gift.nickname === 'scrub') return "../assets/img/scrub.jpg"
-        else return "./assets/img/spray.jpg"
+        else return "../assets/img/spray.jpg"
     }
 
     return (
